66bd5e1958667400ca0a8f31943388cd9421616b
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2022-09-10 09:16:23 +0200

    Init Daily Quest project

---
+++ DailyQuest/DailyQuestUITests/DailyQuestUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DailyQuestUITestsLaunchTests.swift
+//  DailyQuestUITests
+//
+//  Created by Khoi Nguyen on 2022.09.10
+//  Copyright © 2022 Swedebeat AB. All rights reserved.
+//
+
+import XCTest
+
+class DailyQuestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
