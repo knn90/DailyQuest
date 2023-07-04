0330609922a65ea358215a2d626695dfe7d15b5c
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-07-04 10:29:50 +0700

    Navigate to quest details

--- daily_quest/lib/daily_quest_list.dart
+++ daily_quest/lib/daily_quest_list.dart
@@ -51,21 +51,85 @@ class _DailyQuestItemWidgetState extends State<DailyQuestItemWidget> {
   }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Checkbox(
-            checkColor: Colors.white,
-            fillColor: MaterialStateProperty.resolveWith(getColor),
-            value: isDone,
-            onChanged: (bool? value) {
-              setState(() {
-                isDone = value;  
-              });
-          }),
-          Text(widget.title),
-        ],
+    return GestureDetector(
+      child: Center(
+        child: Row(
+          children: [
+            Checkbox(
+              checkColor: Colors.white,
+              fillColor: MaterialStateProperty.resolveWith(getColor),
+              value: isDone,
+              onChanged: (bool? value) {
+                setState(() {
+                  isDone = value;  
+                });
+            }),
+            Text(widget.title),
+          ],
+        ),
       ),
+      onTap: () {
+        Navigator
+        .of(context)
+        .push(MaterialPageRoute(
+          builder: (context) => DailyQuestDetails(quest: widget.title,)
+          )
+        );
+      },
+    );
+  }
+}
+
+class DailyQuestDetails extends StatefulWidget {
+  const DailyQuestDetails({super.key, required this.quest});
+
+  final String quest;
+
+  @override
+  State<DailyQuestDetails> createState() => _DailyQuestDetailsState();
+}
+
+class _DailyQuestDetailsState extends State<DailyQuestDetails> {
+
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.quest);
+    _descriptionController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _descriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quest Details")
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _titleController,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Description",
+              ),
+              controller: _descriptionController,
+              maxLines: 5,
+            )
+          ],
+        ),
     );
   }
 }
--- daily_quest/lib/main.dart
+++ daily_quest/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DailyQuestList(title: "Test Screen",),
+      home: DailyQuestList(title: "Test Screen"),
     );
   }
 }
