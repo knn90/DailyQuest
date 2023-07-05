53205a7c0b5f91807fa3add99c07f293652ecd79
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-07-05 15:34:46 +0700

    Implement swipe to delete

--- daily_quest/lib/quest.dart
+++ daily_quest/lib/quest.dart
@@ -29,7 +29,7 @@ class QuestListNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
-  void update(Quest? quest) {
+  update(Quest? quest) {
     if (quest == null) return;
     final updatedIndex = _items.indexOf(quest);
     _items[updatedIndex] = quest;
@@ -39,5 +39,9 @@ class QuestListNotifier extends ChangeNotifier {
   toogleQuest({required int index}) {
     _items[index].isDone = !_items[index].isDone;
     notifyListeners();
+  }
+
+  remove({required Quest quest}) {
+    _items.remove(quest);
   }
 }
--- daily_quest/lib/quest_list.dart
+++ daily_quest/lib/quest_list.dart
@@ -2,7 +2,6 @@ import 'package:daily_quest/add_quest.dart';
 import 'package:daily_quest/edit_quest.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'quest_details.dart';
 import 'quest.dart';
 
 class DailyQuestList extends StatelessWidget {
@@ -22,25 +21,32 @@ class DailyQuestList extends StatelessWidget {
               itemCount: quests.items.length,
               itemBuilder: (context, index) {
                 var quest = quests.items[index];
-                return ListTile(
-                    leading: Checkbox(
-                      checkColor: Colors.white,
-                      fillColor: MaterialStateProperty.all(Colors.blue),
-                      value: quest.isDone,
-                      onChanged: (value) => context
-                          .read<QuestListNotifier>()
-                          .toogleQuest(index: index),
-                    ),
-                    title: Text(quest.title),
-                    onTap: () {
-                      Navigator.of(context).push<Quest>(
-                        MaterialPageRoute(
-                          builder: (context) => EditQuest(
-                            quest: quest,
+                return Dismissible(
+                  key: Key("${quest.title}_${quest.description}"),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) =>
+                      {context.read<QuestListNotifier>().remove(quest: quest)},
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                      leading: Checkbox(
+                        checkColor: Colors.white,
+                        fillColor: MaterialStateProperty.all(Colors.blue),
+                        value: quest.isDone,
+                        onChanged: (value) => context
+                            .read<QuestListNotifier>()
+                            .toogleQuest(index: index),
+                      ),
+                      title: Text(quest.title),
+                      onTap: () {
+                        Navigator.of(context).push<Quest>(
+                          MaterialPageRoute(
+                            builder: (context) => EditQuest(
+                              quest: quest,
+                            ),
                           ),
-                        ),
-                      );
-                    });
+                        );
+                      }),
+                );
               },
             ),
           ),
