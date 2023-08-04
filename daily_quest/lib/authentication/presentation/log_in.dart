45537942dcf717fa242bc66bba6f54056beb9076
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-08-04 21:31:35 +0700

    Put colum in a scrollview so it scrollable when keyboard appear

--- daily_quest/lib/authentication/presentation/log_in.dart
+++ daily_quest/lib/authentication/presentation/log_in.dart
@@ -14,72 +14,77 @@ class LoginOption extends ConsumerWidget {
     final theme = Theme.of(context);
     final strings = Strings.of(context);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 32.0, vertical: 5),
-              child: Text(strings.signin,
-                  style: theme.textTheme.headlineSmall
-                      ?.copyWith(fontWeight: FontWeight.bold)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const EmailLogin(),
-                SignInButton.email(context: context, onPressed: () {}),
-                const SizedBox(height: 20),
-                _orDivider(context),
-                const SizedBox(height: 20),
-                SignInButton.google(
-                  context: context,
-                  onPressed: () {
-                    ref.read(loginProvider.notifier).signInWithGoogle();
-                  },
-                ),
-                const SizedBox(height: 10),
-                SignInButton.apple(context: context, onPressed: () {}),
-                const SizedBox(height: 10),
-                SignInButton.guest(context: context, onPressed: () {}),
-                const SizedBox(height: 10),
-                _signUpButton(context),
-              ],
-            ),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32.0, vertical: 5),
+                child: Text(strings.signin,
+                    style: theme.textTheme.headlineSmall
+                        ?.copyWith(fontWeight: FontWeight.bold)),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const EmailLogin(),
+                  SignInButton.email(context: context, onPressed: () {}),
+                  const SizedBox(height: 20),
+                  _orDivider(context),
+                  const SizedBox(height: 20),
+                  SignInButton.google(
+                    context: context,
+                    onPressed: () {
+                      ref.read(loginProvider.notifier).signInWithGoogle();
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  SignInButton.apple(context: context, onPressed: () {}),
+                  const SizedBox(height: 10),
+                  SignInButton.guest(context: context, onPressed: () {}),
+                  const SizedBox(height: 10),
+                  _signUpButton(context),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Row _orDivider(BuildContext context) {
+Widget _orDivider(BuildContext context) {
   final theme = Theme.of(context);
   final color = theme.colorScheme.onPrimaryContainer;
   final strings = Strings.of(context);
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Expanded(
-        child: Divider(
-          color: color,
-          endIndent: 10,
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Divider(
+            color: color,
+            endIndent: 10,
+          ),
         ),
-      ),
-      Text(
-        strings.or,
-        style: theme.textTheme.bodyMedium?.copyWith(color: color),
-      ),
-      Expanded(
-        child: Divider(
-          color: color,
-          indent: 10,
+        Text(
+          strings.or,
+          style: theme.textTheme.bodyMedium?.copyWith(color: color),
         ),
-      ),
-    ],
+        Expanded(
+          child: Divider(
+            color: color,
+            indent: 10,
+          ),
+        ),
+      ],
+    ),
   );
 }
 
