88974bab1657e1480eb27c8891a3aef126d075c4
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-08-17 17:17:01 +0700

    Init FirebaseUsersStore

---
+++ daily_quest/lib/authentication/data/datasource/firebase_users_store.dart
@@ -0,0 +1,9 @@
+import 'package:daily_quest/authentication/data/datasource/users_store.dart';
+
+final class FirebaseUsersStore implements UsersStore {
+  @override
+  Future<void> createIfNotExistUser() {
+    // TODO: implement createIfNotExistUser
+    throw UnimplementedError();
+  }
+}
---
+++ daily_quest/test/authentication/data/datasource/firebase_users_store_test.dart
@@ -0,0 +1,5 @@
+import 'package:daily_quest/authentication/data/datasource/firebase_users_store.dart';
+
+void main() {
+  late FirebaseUsersStore sut;
+}
