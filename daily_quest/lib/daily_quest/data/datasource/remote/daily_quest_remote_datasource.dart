a19bf08ca4248fd084eb9ac24a4ae7c94c4136aa
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-08-25 16:03:57 +0700

    Remove task related method

--- daily_quest/lib/daily_quest/data/datasource/remote/daily_quest_remote_datasource.dart
+++ daily_quest/lib/daily_quest/data/datasource/remote/daily_quest_remote_datasource.dart
@@ -1,15 +1,7 @@
 import 'package:daily_quest/daily_quest/data/model/remote/remote_daily_quest.dart';
-import 'package:daily_quest/daily_quest/data/model/remote/remote_task.dart';
 
 abstract class DailyQuestRemoteDataSource {
   Future<RemoteDailyQuest?> getTodayQuest();
   Future<void> createQuest({required RemoteDailyQuest quest});
   Future<void> updateQuest({required RemoteDailyQuest quest});
-  Future<RemoteDailyQuest> addTask({required RemoteTask task});
-  Future<RemoteDailyQuest> editTask(
-      {required RemoteTask task, required int index});
-  Future<RemoteDailyQuest> toggleTask({required int index});
-  Future<RemoteDailyQuest> removeTask({required int index});
-  Future<RemoteDailyQuest> moveTask(
-      {required int fromIndex, required int toIndex});
 }
--- daily_quest/lib/daily_quest/data/datasource/remote/firebase_remote_datasource.dart
+++ daily_quest/lib/daily_quest/data/datasource/remote/firebase_remote_datasource.dart
@@ -1,6 +1,5 @@
 import 'package:daily_quest/daily_quest/data/datasource/remote/daily_quest_remote_datasource.dart';
 import 'package:daily_quest/daily_quest/data/model/remote/remote_daily_quest.dart';
-import 'package:daily_quest/daily_quest/data/model/remote/remote_task.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 final class FirebaseRemoteDataSource implements DailyQuestRemoteDataSource {
@@ -16,18 +15,6 @@ final class FirebaseRemoteDataSource implements DailyQuestRemoteDataSource {
         _timestamp = timestamp,
         _dailyQuestRef =
             dailyQuestRef ?? FirebaseDatabase.instance.ref('daily_quests');
-  @override
-  Future<RemoteDailyQuest> addTask({required RemoteTask task}) {
-    // TODO: implement addTask
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<RemoteDailyQuest> editTask(
-      {required RemoteTask task, required int index}) {
-    // TODO: implement editTask
-    throw UnimplementedError();
-  }
 
   @override
   Future<RemoteDailyQuest?> getTodayQuest() async {
@@ -44,25 +31,6 @@ final class FirebaseRemoteDataSource implements DailyQuestRemoteDataSource {
   @override
   Future<void> createQuest({required RemoteDailyQuest quest}) async {
     await _dailyQuestRef.child(_userId).child(_timestamp).set(quest.toJson());
-  }
-
-  @override
-  Future<RemoteDailyQuest> moveTask(
-      {required int fromIndex, required int toIndex}) {
-    // TODO: implement moveTask
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<RemoteDailyQuest> removeTask({required int index}) {
-    // TODO: implement removeTask
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<RemoteDailyQuest> toggleTask({required int index}) {
-    // TODO: implement toggleTask
-    throw UnimplementedError();
   }
 
   @override
