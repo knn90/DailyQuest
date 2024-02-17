0a43e9815b6cecd1545030df69858362509ed830
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2024-02-17 14:33:12 +0700

    Create DailyQuest iOS project

---
+++ DailyQuest/DailyQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuestApp.swift
+//  DailyQuest
+//
+//  Created by Khoi Nguyen on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
